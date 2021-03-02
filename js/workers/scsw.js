0876fe0fe1d5d94d05993c1e46378ab1724d88db
Maygician <maygicinc@gmail.com>
2021-03-03 06:41:49 +0700

    sw push try

--- js/workers/scsw.js
+++ js/workers/scsw.js
@@ -5,7 +5,7 @@ var urlsToCache = []
 //   '/styles/main.css',
 //   '/script/main.js'
 // ];
-
+//#region install
 self.addEventListener('install', function(event) {
     // Perform install steps
     event.waitUntil(
@@ -16,3 +16,55 @@ self.addEventListener('install', function(event) {
         })
     );
   });
+  /*self.addEventListener('install', (event) => {
+    event.waitUntil(async function() {
+      const cache = await caches.open('mygame-core-v1');
+      cache.addAll(
+        // levels 11-20
+      );
+      await cache.addAll(
+        // core assets & levels 1-10
+      );
+    }());
+  });*/
+  //#endregion
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(async function() {
+      const cacheNames = await caches.keys();
+      await Promise.all(
+        cacheNames.filter((cacheName) => {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+          if(cacheName=="toDelete")
+          console.log('cache deleted')
+          return true;
+        }).map(cacheName => caches.delete(cacheName))
+      );
+    }());
+  });
+//#region push
+self.addEventListener('push', (event) => {
+  if (event.data.text() == 'new-email') {
+    event.waitUntil(async function() {
+      const cache = await caches.open('mysite-dynamic');
+      const response = await fetch('/inbox.json');
+      await cache.put('/inbox.json', response.clone());
+      const emails = await response.json();
+      registration.showNotification("New email", {
+        body: "From " + emails[0].from.name,
+        tag: "new-email"
+      });
+    }());
+  }
+});
+
+self.addEventListener('notificationclick', function(event) {
+  if (event.notification.tag == 'new-email') {
+    // Assume that all of the resources needed to render
+    // /inbox/ have previously been cached, e.g. as part
+    // of the install handler.
+    new WindowClient('/inbox/');
+  }
+});
+//#region 
