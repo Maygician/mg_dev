9316c7af1bdeb54e2318d906aba9e87275b7736b
Maygician <maygicinc@gmail.com>
2021-03-03 14:11:55 +0700

    ww local imports

--- js/workers/schat.js
+++ js/workers/schat.js
@@ -1,3 +1,5 @@
+self.importScripts("/js/cdn/gun.js", "/js/cdn/sea.js")
+
 onmessage = function(e) {
     console.log(' got msg from the main script');
     var workerResult = 'result: ' + (e.data[0] * e.data[1]);
